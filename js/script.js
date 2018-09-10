2c89a6e3c7bf0c99e6b48bcb7ee550c6495851d9
EJ Serafin <edgardo@chainblx.com>
2018-09-10 12:22:33 -0700

    updated the keys in each object
    source and citation needed to be updated in each object

--- js/script.js
+++ js/script.js
@@ -4,62 +4,68 @@
 var quotes = [
     {
         quote: "Frankly my dear, I don't give a damn.",
-        source: 'Gone with the Wind',
+        source: 'Clark Gable',
+        citation: 'Gone with the Wind',
         year: '1939'
     },
     {
-        quote: "Mamam said life is like a box of chocolates, you never know what you're going to get.",
-        source: 'Forrest Gump',
-        year: '1994',
-        actor: 'Tom Hanks'
+        quote: "Mama said life is like a box of chocolates, you never know what you're going to get.",
+        source: 'Tom Hanks',
+        citation: 'Forrest Gump',
+        year: '1994'
     },
     {
         quote: "Mrs. Robinson you're trying to seduce me. Aren't you?",
-        source: "The Graduate",
+        source: "Dustin Hoffman",
+        citation: 'The Graduate',
         year: '1967'
     },
     {
         quote: "Of all the gin joints in all of town, she walks into mine.",
-        source: 'Casablanca',
+        source: 'Humphrey Bogart',
+        citation: 'Casablanca',
         year: '1942'
     },
     {
         quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
-        source: "Dead Poets Society",
-        year: '1989',
-        actor: 'Robin Williams'
+        source: "Robin Williams",
+        citation: "Dead Poets Society",
+        year: '1989'
     },
     {
         quote: "The greatest trick the devil ever pulled was convincing the world he didn't exist.",
-        source: 'The Usual suspects',
+        source: 'Kevin Spacey',
+        citation: 'The Usual suspects',
         year: '1995'
     },
     {
         quote: "We're on a mission from God.",
-        source: 'The Blues Brothers',
+        source: 'Dan Akyroyd',
+        citation: 'The Blues Brothers',
         year: '1980'
     },
     {
         quote: "You make me want to be a better man.",
-        source: 'As Good as It Gets',
+        source: 'Jack Nicholson',
+        citation: 'As Good as It Gets',
         year: '1997'
     },
     {
         quote: "Leave the gun. Take the cannoli.",
-        source: 'The Godfather',
+        source: 'Richard Castellano',
+        citation: 'The Godfather',
         year: '1972'
     },
     {
         quote: "It'd be a lot cooler if you did.",
-        source: 'Dazed and Confused',
-        year: '1993',
-        actor: 'Matthew McConaughey'
+        source: 'Matthew McConaughey',
+        citation: 'Dazed and Confused',
+        year: '1993'
     }
 ];
 
 
-// Create the getRandomQuuote function and name it getRandomQuote
-
+// Create the getRandomQuote function and name it getRandomQuote
 
 
 // Create the printQuote funtion and name it printQuote
