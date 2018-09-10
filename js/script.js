68535a3dc227fec91532849cdd8a2e29de09d8f6
EJ Serafin <edgardo@chainblx.com>
2018-09-10 11:16:18 -0700

    Added the array of quote objects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,61 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
-
+var quotes = [
+    {
+        quote: "Frankly my dear, I don't give a damn.",
+        source: 'Gone with the Wind',
+        year: '1939'
+    },
+    {
+        quote: "Mamam said life is like a box of chocolates, you never know what you're going to get.",
+        source: 'Forrest Gump',
+        year: '1994',
+        actor: 'Tom Hanks'
+    },
+    {
+        quote: "Mrs. Robinson you're trying to seduce me. Aren't you?",
+        source: "The Graduate",
+        year: '1967'
+    },
+    {
+        quote: "Of all the gin joints in all of town, she walks into mine.",
+        source: 'Casablanca',
+        year: '1942'
+    },
+    {
+        quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
+        source: "Dead Poets Society",
+        year: '1989',
+        actor: 'Robin Williams'
+    },
+    {
+        quote: "The greatest trick the devil ever pulled was convincing the world he didn't exist.",
+        source: 'The Usual suspects',
+        year: '1995'
+    },
+    {
+        quote: "We're on a mission from God.",
+        source: 'The Blues Brothers',
+        year: '1980'
+    },
+    {
+        quote: "You make me want to be a better man.",
+        source: 'As Good as It Gets',
+        year: '1997'
+    },
+    {
+        quote: "Leave the gun. Take the cannoli.",
+        source: 'The Godfather',
+        year: '1972'
+    },
+    {
+        quote: "It'd be a lot cooler if you did.",
+        source: 'Dazed and Confused',
+        year: '1993',
+        actor: 'Matthew McConaughey'
+    }
+];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
